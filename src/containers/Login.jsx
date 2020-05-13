381a70e544dd29de467e57416d1d3822f33ed632
Andres Campuzano <campuxano88@gmail.com>
2020-05-13 09:44:12 -0500

    cl 16, form.

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Login.scss';
@@ -7,16 +7,40 @@ import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
 const Login = () => {
+	const [form, setValues] = useState({
+		email: '',
+	});
+
+	const handleInput = (event) => {
+		setValues({
+			...form,
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(form);
+	};
+
 	return (
 		<section className='login'>
 			<section className='login__container'>
 				<h2>Inicia sesión</h2>
-				<form className='login__container--form'>
-					<input className='input' type='text' placeholder='Correo' />
+				<form className='login__container--form' onSubmit={handleSubmit}>
 					<input
+						name='email'
+						className='input'
+						type='text'
+						placeholder='Correo'
+						onChange={handleInput}
+					/>
+					<input
+						name='password'
 						className='input'
 						type='password'
 						placeholder='Contraseña'
+						onChange={handleInput}
 					/>
 					<button className='button'>Iniciar sesión</button>
 					<div className='login__container--remember-me'>
