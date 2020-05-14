8c3dc4bbeb393072fa86d6597e15eb7d4fa091ac
Andres Campuzano <campuxano88@gmail.com>
2020-05-14 05:34:20 -0500

    cl 21. Register.

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Register.scss';
 
-const Register = () => {
+const Register = (props) => {
+	//added props
+	const [form, setValues] = useState({
+		email: '',
+		name: '',
+		password: '',
+	});
+
+	const handleInput = (event) => {
+		setValues({
+			...form,
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(form);
+		props.history.push('/'); //added
+	};
 	return (
 		<section className='register'>
 			<section className='register__container'>
 				<h2>Regístrate</h2>
-				<form className='register__container--form'>
-					<input className='input' type='text' placeholder='Nombre' />
-					<input className='input' type='text' placeholder='Correo' />
+				<form className='register__container--form' onSubmit={handleSubmit}>
 					<input
+						name='name'
+						className='input'
+						type='text'
+						placeholder='Nombre'
+						onChange={handleInput}
+					/>
+					<input
+						name='email'
+						className='input'
+						type='email'
+						placeholder='Correo'
+						onChange={handleInput}
+					/>
+					<input
+						name='password'
 						className='input'
 						type='password'
 						placeholder='Contraseña'
+						onChange={handleInput}
 					/>
 					<button className='button'>Registrarme</button>
 				</form>
