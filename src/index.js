70c47310d3f2f937f0288af72f2ebd44456764df
Andres Campuzano <campuxano88@gmail.com>
2020-05-14 09:50:42 -0500

    cl 27. Redux DevTools.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './routes/App';
 
 // Redux
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
 const initialState = {
@@ -173,7 +173,9 @@ const initialState = {
 	],
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
 	<Provider store={store}>
